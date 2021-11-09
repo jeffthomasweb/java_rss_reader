414b2bccbb50ff4858534d40befc06ee9be1d8df
jeffthomasweb <93561509+jeffthomasweb@users.noreply.github.com>
2021-11-09 14:04:12 -0500

    Deleted unnecessary import

--- app/src/main/java/Readxml/App.java
+++ app/src/main/java/Readxml/App.java
@@ -15,7 +15,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.net.*;
 import java.net.http.*;
